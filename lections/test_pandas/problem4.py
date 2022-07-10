a8318476b3f470adad7625ebb7de150c50948d1a
Anton Shiryaev <med.phisiker@gmail.com>
2022-07-11 03:29:34 +1000

    delete

--- lections/test_pandas/problem4.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import pandas as pd
-
-df = pd.read_csv('lections\\test_pandas\\input4.csv')
-corr = df.corr().drop('Unnamed: 0').drop('Unnamed: 0', axis=1)
-corr = corr.abs().max(axis=0)
-print(corr.tolist())
