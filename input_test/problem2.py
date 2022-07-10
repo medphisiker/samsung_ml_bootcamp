4db13a5dd635b6ec9227a9da91f45ada43351b0a
Anton Shiryaev <med.phisiker@gmail.com>
2022-07-10 15:46:57 +1000

    init

---
+++ input_test/problem2.py
@@ -0,0 +1,27 @@
+# 0 1 2 3 4
+# Заполните X так, чтобы после выполнения данной
+# программы, переменная z содержала значение 0.
+
+# z = X
+
+# def summa(x, y):
+
+#     z = x + y
+
+#     return None
+
+# summa(X, X)
+
+
+z = 0
+
+
+def summa(x, y):
+
+    z = x + y
+
+    return None
+
+
+summa(2, 3)
+print(z)
---
+++ input_test/problem3.py
@@ -0,0 +1,18 @@
+# Какое число нужно поставить вместо X, чтобы данная
+# программа напечатала 6 строк?
+
+# for i in range(1, X):
+#     i = 10
+#     print(i)
+#     i = 11
+#     print(i)
+#     i = 12
+#     print(i)
+
+for i in range(1, 3):
+    i = 10
+    print(i)
+    i = 11
+    print(i)
+    i = 12
+    print(i)
